423d24978774791e8c0190d64312639f9981a660
ranajollanar <ranajollanarbh@gmail.com>
2025-01-25 12:46:18 +0100

    Added voice manager script

--- MVP_MMT/Assets/Scripts/VoiceManager.cs
+++ MVP_MMT/Assets/Scripts/VoiceManager.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using Meta.WitAi.CallbackHandlers;
+using Oculus.Voice;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class VoiceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Wit Configuration")]
+    [SerializeField] private AppVoiceExperience appVoiceExperience;
+    [SerializeField] private TextMeshProUGUI transcriptionText;
+
+    [Header("Voice Events")]
+    [SerializeField] private UnityEvent<string> completeTranscription;
+
+    [Header("Listening Configuration")]
+    [SerializeField] private float listeningDuration = 5f; // Default duration in seconds.
+
+    private Coroutine listeningCoroutine;
+
+    /// <summary>
+    /// Method to activate voice listening. Call this on button click.
+    /// </summary>
+    public void StartListening()
     {
-        
+        if (listeningCoroutine != null)
+        {
+            StopCoroutine(listeningCoroutine);
+        }
+
+        appVoiceExperience.Activate();
+        listeningCoroutine = StartCoroutine(StopListeningAfterDuration());
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Stop listening after the specified duration.
+    /// </summary>
+    private IEnumerator StopListeningAfterDuration()
     {
-        
+        yield return new WaitForSeconds(listeningDuration);
+        appVoiceExperience.Deactivate();
+    }
+
+    private void OnEnable()
+    {
+        appVoiceExperience.VoiceEvents.OnPartialTranscription.AddListener(OnPartialTranscription);
+        appVoiceExperience.VoiceEvents.OnFullTranscription.AddListener(OnFullTranscription);
+    }
+
+    private void OnDisable()
+    {
+        appVoiceExperience.VoiceEvents.OnPartialTranscription.RemoveListener(OnPartialTranscription);
+        appVoiceExperience.VoiceEvents.OnFullTranscription.RemoveListener(OnFullTranscription);
+    }
+
+    private void OnPartialTranscription(string transcription)
+    {
+        transcriptionText.text = transcription;
+    }
+
+    private void OnFullTranscription(string transcription)
+    {
+        completeTranscription?.Invoke(transcription);
+    }
+
+    /// <summary>
+    /// Updates the listening duration dynamically.
+    /// </summary>
+    /// <param name="duration">New duration in seconds.</param>
+    public void SetListeningDuration(float duration)
+    {
+        listeningDuration = duration;
     }
 }
